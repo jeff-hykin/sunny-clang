e806dbdbab278e40af6d7d4b3077e73da513f862
Jeff Hykin <jeff.hykin@gmail.com>
2025-11-17 07:20:53 -0600

    format

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,17 @@
 /**
  * Counter module.
  */
-import { React } from "../../deps.ts";
+import { React } from "../../deps.ts"
 
 export default function Counter() {
-  // @ts-ignore Function is a named export but TS can't find it.
-  const [count, setCount] = React.useState(0);
+    // @ts-ignore Function is a named export but TS can't find it.
+    const [count, setCount] = React.useState(0)
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
+    return (
+        <div>
+            <p>You clicked {count} times</p>
 
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    )
 }
